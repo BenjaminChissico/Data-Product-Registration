cd0d926de1dd51a825a2265f8d76e2a27941dbf6
benjamin.chissico <benjamin.chissico@uniqa.at>
2022-12-30 10:26:33 +0100

    init commit

---
+++ main.py
@@ -0,0 +1,11 @@
+"""This modules serves as entry point into the application"""
+import src.app 
+
+def main():
+    
+
+    src.app.create_app()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/app.py
@@ -0,0 +1,7 @@
+def create_app():
+    pass 
+
+
+
+if __name__ == '__main__':
+    create_app()
