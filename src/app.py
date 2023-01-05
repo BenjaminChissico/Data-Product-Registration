fe64197cb1ba07d0ab7a6644d6a9cff74434be86
Benjamin.Chissico <benjamin.chissico@gmail.com>
2023-01-05 09:59:21 +0100

    added docker capabilities

--- src/app.py
+++ src/app.py
@@ -3,59 +3,48 @@ the create_app function takes care of the app creation and running and
 can be imported into other modules.
 """
 
-import os 
+import os
 import streamlit as st
 import src.modules.ui_components.dp_form as dp_form
 import src.modules.logic.blob_storage as bs
 
 
-# only needed for local testing 
+# only needed for local testing
 from dotenv import load_dotenv
 
 
-
 def create_app():
-    st.set_page_config(layout="wide",page_title="Data Product Ingestion")
-    st.markdown("<h1 align=middle>Welcome to the Data Shop Ingestion Application</h1>",unsafe_allow_html=True)
+    st.set_page_config(layout="wide", page_title="Data Product Ingestion")
+    st.markdown(
+        "<h1 align=middle>Welcome to the Data Shop Ingestion Application</h1>",
+        unsafe_allow_html=True,
+    )
     # will hold all tutorial information later, either youtube video or saved movie clip in storage
     with st.expander("""New here? Watch the tutorial, then :point_down:"""):
         url = "https://www.youtube.com/watch?v=gy1B3agGNxw"
         st.video(url)
 
-
-    # only for local development 
+    # only for local development
     load_dotenv()
 
-    azure_account_url = os.environ['AZURE_ACCOUNT_URL']
-    azure_container_name = os.environ['DATA_PRODUCTS_CONTAINER_NAME']
-    blob_handler = bs.BlobStorage(azure_account_url,azure_container_name)
-    
+    azure_account_url = os.environ["AZURE_ACCOUNT_URL"]
+    azure_container_name = os.environ["DATA_PRODUCTS_CONTAINER_NAME"]
+    blob_handler = bs.BlobStorage(azure_account_url, azure_container_name)
 
-    # get all the information of the data product 
-    dp_details = dp_form.data_product_form(blob_handler)    
+    # get all the information of the data product
+    dp_details = dp_form.data_product_form(blob_handler)
     file = dp_form.upload_data_product()
 
-    # read the content of the data product and create details 
-    # afterwards push the data product to azure 
+    # read the content of the data product and create details
+    # afterwards push the data product to azure
 
-    # init azure account details 
-    
-    filled_dp_details = dp_form.register_product(file,dp_details,blob_handler)
-    
-    #TODO needs to be pushed to the front-end
-    json_details  = filled_dp_details.whole_data_product_to_dict()
+    # init azure account details
+
+    filled_dp_details = dp_form.register_product(file, dp_details, blob_handler)
+
+    # TODO needs to be pushed to the front-end
+    json_details = filled_dp_details.whole_data_product_to_dict()
     st.success("Succesfully uploaded to azure & here are the details")
-    
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
